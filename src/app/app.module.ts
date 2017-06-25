8a9f14970fd88d133e1dd9421ac6ba80b85fcc9f
Paulo Henrique Gomes <paulogomes.wd@gmail.com>
2017-06-25 15:12:12 -0300

    Adding firebase:   * npm install firebase angularfire2 --save   * Adding and configuring the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAST9UHyZOqoH4V_m2nVUhFZqfCHlTf6qQ",
+    authDomain: "pepper-48d41.firebaseapp.com",
+    databaseURL: "https://pepper-48d41.firebaseio.com",
+    projectId: "pepper-48d41",
+    storageBucket: "",
+    messagingSenderId: "109834548692"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
